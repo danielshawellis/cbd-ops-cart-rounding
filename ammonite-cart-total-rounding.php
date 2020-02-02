27b0e2e0f86fd8db8fd2adccb1714aec469ea3fd
Daniel Ellis <danielshawellis@gmail.com>
2020-02-02 15:26:34 -0600

    add plugin headers and meta information

---
+++ ammonite-cart-total-rounding.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Plugin Name:       Ammonite Cart Total Rounding
+ * Description:       Round WooCommerce cart totals to the nearest dollar.
+ * Version:           1.0.0
+ * Author:            Daniel Ellis
+ * Author URI:        https://danielellisdevelopment.com/
+ */
